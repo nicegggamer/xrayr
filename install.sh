7e8f59f447f60513cdd143f4783b13d64509d1ce
Đỗ Minh Trí <103892814+dominhtri2007@users.noreply.github.com>
2022-04-22 19:39:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,6 @@ red='\033[0;31m'
 green='\033[0;32m'
 #yellow='\033[0;33m'
 plain='\033[0m'
-operation=(Install Update UpdateConfig logs restart delete)
-# Đảm bảo rằng chỉ người chủ mới có thể chạy tập lệnh của chúng tôi
 [[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] Chưa vào root kìa !, vui lòng xin phép ROOT trước!" && exit 1
 
 # Kiểm tra hệ thống
